604d6a1a44d455b5ff1f4e38c1d2134769580e85
Joshua Moore <joshua92599@gmail.com>
2022-10-22 10:46:32 -0400

    added to the Person Class

--- Hackathon2022/hackathon2022/src/prob3/Person.java
+++ Hackathon2022/hackathon2022/src/prob3/Person.java
@@ -2,4 +2,24 @@ package prob3;
 
 public class Person {
 
+	private String name;
+	private int age;
+//	private Report rep;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 }
