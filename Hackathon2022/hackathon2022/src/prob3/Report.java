72b280032c10513a02f2a486f00272bd73088d39
Joshua Moore <joshua92599@gmail.com>
2022-10-22 12:11:16 -0400

    sorted selfs codes

---
+++ Hackathon2022/hackathon2022/src/prob3/Report.java
@@ -0,0 +1,27 @@
+package prob3;
+
+public abstract class Report {
+	protected Pain pain;
+	protected Drowsiness drowsiness;
+	protected MentalHealth mentalHealth;
+	
+	public Report(Drowsiness drowsiness) {
+		
+		this.drowsiness = drowsiness;
+		
+	}
+	public Report(Pain pain) {
+		this.pain = pain;
+	}
+	public Report(MentalHealth mentalHealth) {
+		this.mentalHealth = mentalHealth;
+			
+		}
+	public Object getReport(Object o) {
+		return o;
+	}
+	@Override
+	public String toString(){
+		return getReport();
+	}
+}
