de36048f3c100c2577a1c0d6ed0fc85338d28d38
Joshua Moore <joshua92599@gmail.com>
2022-10-22 14:52:40 -0400

    add test meth

--- Hackathon2022/hackathon2022/src/prob3/PersonHandlerTest.java
+++ Hackathon2022/hackathon2022/src/prob3/PersonHandlerTest.java
@@ -2,4 +2,9 @@ package prob3;
 
 public class PersonHandlerTest {
 
+	public void testPrintReporttxt() {
+		Person p = new Person("Stevo", 69);
+		PersonHandler ph = new PersonHandler();
+		ph.addPerson(p);
+	}
 }
