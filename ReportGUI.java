78052c4e2a0abd056604c1713d3fa4bcf8723d6d
Gio <giovanniahernandez20@gmail.com>
2022-10-22 10:53:54 -0400

    Added Additional Components

--- ReportGUI.java
+++ ReportGUI.java
@@ -1,32 +1,35 @@
 import java.awt.event.*;
 import java.awt.TextField; 
-import java.awt.TextArea;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.FlowLayout;
 import java.awt.Button;
 
 public class ReportGUI implements ActionListener {
-    TextArea ta;
-    TextField tf;
-    Label lbl;
+    
+    TextField tf1, tf2, tf3;
+    Label lbl1, lbl2, lbl3;
     Button btn1, btn2, btn3;
     Frame frame;
 
     public ReportGUI () {
         frame = new Frame("Report Program");
-        ta = new TextArea();
-        tf = new TextField(20);
-        lbl = new Label();
+        
+        tf1 = new TextField(20);
+        tf2 = new TextField(20);
+        tf3 = new TextField(20);
+        lbl1 = new Label("Name");
+        lbl2 = new Label("Age");
+        lbl3 = new Label("Type of Report");
 
-        frame.setLayout( new FlowLayout() );
-        frame.add(ta);
-        frame.add(tf);
-        /* 
-        frame.add(btn1);
-        frame.add(btn2);
-        frame.add(btn3);
-        */
+        frame.setLayout( new FlowLayout( FlowLayout.LEFT) );
+        
+        frame.add(lbl1);
+        frame.add(tf1);
+        frame.add(lbl2);
+        frame.add(tf2);
+        frame.add(lbl3);
+        frame.add(tf3);
         
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent we) {
